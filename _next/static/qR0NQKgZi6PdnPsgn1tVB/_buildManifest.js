8dbbd395d9739099dd0e2aebe0e64c66fdb1aa0f
Akram Yoonos <Akramyoonos006@gmail.com>
2026-01-13 10:43:55 +0530

    Updates

---
+++ _next/static/qR0NQKgZi6PdnPsgn1tVB/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/A7-Vault//_next/:path+",
+        "destination": "/A7-Vault/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
